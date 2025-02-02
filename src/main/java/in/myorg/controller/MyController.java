1c261e6679597f8032de7452af7d3abf59090b8c
SameerMohanty7846 <sameermohanty6369@gmail.com>
2025-02-02 11:38:28 +0530

    updated

--- src/main/java/in/myorg/controller/MyController.java
+++ src/main/java/in/myorg/controller/MyController.java
@@ -5,8 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
-	@GetMapping("/")
-	public String welcomeView() {
+	@GetMapping(path= {"welcome","home",""})
+	public String viewWelcome() {
 		return "index";
 	}
+	@GetMapping("about")
+	public String viewAbout() {
+		return "about";
+	}
+	@GetMapping("cars")
+	public String viewCars() {
+		return "cars";
+	}
+	@GetMapping("services")
+	public String viewServices() {
+		return "services";
+	}
+	@GetMapping("contacts")
+	public String viewContacts() {
+		return "contacts";
+	}
+	
+	
+	
 }
